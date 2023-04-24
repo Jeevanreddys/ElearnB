45d6859cb901f88046d0913222f1b0a4345a79dc
hemanthgara <hemanthgara.hg@gmail.com>
2023-04-23 20:55:18 -0400

    Push notifications code added in helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -22,3 +22,43 @@ exports.handleError = (error, status = 400, res,) => {
         })
     }
 }
+
+exports.sendMailer = async (email, subject, message, res) => {
+
+    const transporter = nodemailer.createTransport({
+        host: `${process.env.SMPT_EMAIL_HOST}`,
+        port: `${process.env.SMPT_EMAIL_PORT}`,
+        auth: {
+            user: `${process.env.SMPT_EMAIL_USER}`,
+            pass: `${process.env.SMPT_EMAIL_PASSWORD}`
+        },
+        // secure: false
+    })
+
+    const data = {
+        from: `${process.env.SMPT_EMAIL_FROM}`,
+        to: `${email}`,
+        subject: `${subject} - E-learning`,
+        html: `${message}`,
+    }
+
+    transporter.sendMail(data, (error, info) => {
+        if (error) {
+            console.log('error>>>>>>', error);
+            res.status(error.responseCode).send(error)
+        }
+    })
+
+    return
+}
+
+exports.createUUID = () => {
+    var dt = new Date().getTime()
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0
+        dt = Math.floor(dt / 16)
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+    })
+
+    return uuid
+}
