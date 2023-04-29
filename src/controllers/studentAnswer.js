a772549c6ddf185c9f324cf6bb28552cf3be48de
hemanthgara <hemanthgara.hg@gmail.com>
2023-04-28 23:17:51 -0400

    Backend code added to post the student answers

---
+++ src/controllers/studentAnswer.js
@@ -0,0 +1,25 @@
+const { StudentAnswer } = require("../models")
+const { handleResponse, handleError, getPercentage } = require("../utils/helpers")
+
+
+exports.getOverAllScore = async (req, res) => {
+
+    await StudentAnswer.find({ user_id: req.user.id, course_id: req.params.course_id }).then(async (data) => {
+        const total_question = []
+        const score = []
+        let course_name = ''
+
+      
+
+        const percent = getPercentage(total_question, score)
+
+        const dt = {
+            percentage: parseFloat(percent), name: req.user.first_name + ' ' + req.user.last_name, course_name: course_name
+        }
+
+        handleResponse(res, dt, 200)
+
+    }).catch(err => {
+        handleError(err.message, 400, res)
+    })
+}
