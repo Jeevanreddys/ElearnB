82e3736f3c9028703ac023ce0424d6340278468d
hemanthgara <hemanthgara.hg@gmail.com>
2023-04-30 12:09:42 -0400

    New routing added for media section

--- src/routes/media.js
+++ src/routes/media.js
@@ -6,10 +6,12 @@ const path = require("path")
 module.exports = app => {
 
     router.get('/media/:name', (req, res) => {
-
+        router.get('/media/:type', (req, res) => {
         const { type, name } = req.params
+        const { type, type } = req.params
 
         res.sendFile(path.join(__dirname, `../upload/${name}`,))
+        res.sendFile(path.join(__dirname, `../upload/${type}`,))
         
     })
 
